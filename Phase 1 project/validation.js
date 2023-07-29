e33b325243d037a7bc22fbde15b83d3c7148d1b0
Devananda27200 <84342814+Devananda27200@users.noreply.github.com>
2023-07-29 13:31:15 +0530

    Add files via upload

--- Phase 1 project/validation.js
+++ Phase 1 project/validation.js
@@ -36,7 +36,7 @@ function validateMobileNumber() {
 }
 //--------------------------------------------------------------------------------------------------------------
 function validateUsername() {
-  const usernameInput = document.getElementById('username');
+  const usernameInput = document.getElementById('username1');
   const usernameError = document.getElementById('usernameError');
   const username = usernameInput.value.trim();
   const disallowedCharsPattern = /[^\w-.]/;
@@ -60,7 +60,7 @@ function validateUsername() {
 
 //--------------------------------------------------------------------------------------------------------------
 function validatePassword() {
-  const passwordInput = document.getElementById('password');
+  const passwordInput = document.getElementById('password1');
   const passwordError = document.getElementById('passwordError');
   const password = passwordInput.value.trim();
 
@@ -96,19 +96,20 @@ function validateLastName() {
     lastNameError.textContent = '';
   }
 }
+
 //--------------------------------------------------------------------------------------------------------------
 function validatePasswordMatch() {
   const passwordInput = document.getElementById('password');
   const confirmPasswordInput = document.getElementById('confirmPassword');
-  const passwordError = document.getElementById('passwordError');
+  const confirmPasswordError = document.getElementById('passwordError');
 
   const password = passwordInput.value;
   const confirmPassword = confirmPasswordInput.value;
 
  if (password !== confirmPassword) {
-    passwordError.textContent = 'Passwords do not match.';
+    confirmPasswordError.textContent = 'Passwords do not match.';
   } else {
-    passwordError.textContent = '';
+    confirmPasswordError.textContent = '';
   }
 }
 
@@ -119,13 +120,13 @@ function validateForm(event) {
   validatePasswordMatch();
 
   // If there are no errors, submit the form
-  if (passwordError.textContent === '') {
+  if (confirmPasswordError.textContent === '') {
     event.target.submit();
   }
 }
 
 const form = document.querySelector('form');
-const passwordInput = document.getElementById('password');
+const passwordInput = document.getElementById('password1');
 const confirmPasswordInput = document.getElementById('confirmPassword');
 
 // Validate on input change
@@ -136,4 +137,79 @@ confirmPasswordInput.addEventListener('input', validatePasswordMatch);
 form.addEventListener('submit', validateForm);
 
 //--------------------------------------------------------------------------------------------------------------
+//validation of login username 
+function validateUsername1()
+{
+            const usernameInput = document.getElementById('username');
+            const username = usernameInput.value.trim();
+            const usernameErrorSpan = document.getElementById('usernameError');
 
+            const validUsernameRegex = /^[a-zA-Z0-9_]{4,20}$/;
+
+            if (validUsernameRegex.test(username))
+            {
+                usernameErrorSpan.textContent = '';
+            }
+            else
+            {
+                usernameErrorSpan.textContent = 'Username must be 4 to 20 characters long and can only contain letters, numbers, and underscores.';
+            }
+}
+
+//--------------------------------------------------------------------------------------------------------------
+//login password
+function validatePassword1() {
+    const passwordInput = document.getElementById('password');
+    const password = passwordInput.value;
+
+    const passwordErrorSpan = document.getElementById('passwordError');
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const digitRegex = /\d/;
+
+    if (password.length >= 8 &&
+        uppercaseRegex.test(password) &&
+        lowercaseRegex.test(password) &&
+        digitRegex.test(password)) {
+        passwordErrorSpan.textContent = '';
+    } else {
+        passwordErrorSpan.textContent = 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.';
+    }
+}
+//--------------------------------------------------------------------------------------------------------------
+function disableFutureDates() {
+// Get the input element for the selected date
+        const selectedDateInput = document.getElementById('selectedDate');
+
+        // Get today's date
+        const today = new Date();
+        const todayString = today.toISOString().split('T')[0];
+
+        // disable future dates
+
+        selectedDateInput.max = todayString;
+}
+
+//---------------------------------------------------------------------------------------------------------
+ function calculateAge() {
+            const birthdateInput = document.getElementById('birthdate');
+            const ageInput = document.getElementById('age');
+
+            const birthdate = new Date(birthdateInput.value);
+            const today = new Date();
+            const ageInMilliseconds = today - birthdate;
+            const ageDate = new Date(ageInMilliseconds);
+            const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+            if (age >= 18) {
+                ageInput.value = age;
+            }
+            else
+            {
+                ageInput.value = '';
+                alert("You must be at least 18 years old.");
+            }
+               const age1 = document.getElementById('birthdate').addEventListener('input', calculateAge);
+
+        }
+//----------------------------------------------------------------------------------------------------------------------------------
